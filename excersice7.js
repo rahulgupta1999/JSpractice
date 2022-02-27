63f008b31c832cec4343e3b17a205474f0eecb7c
rahul.gupta <rahul.gupta@rakuten.com>
2022-02-28 03:55:52 +0900

    excersice 7

---
+++ excersice7.js
@@ -0,0 +1,31 @@
+let x = Math.random(); //generate random no
+a=Number("3.14")    // returns 3.14
+console.log(a)
+a=Number(" ")       // returns 0
+console.log(a)
+a=Number("")        // returns 0
+console.log(a)
+a=Number("99 88")  //Nan
+
+
+b="12.3" //string of value 12.3
+b=parseFloat(b) // b is now float having value 12.3
+parseFloat("10 20 30");  // returns 10
+parseFloat("10 years");  // returns 10
+parseFloat("years 10");  // returns NaN
+a = 12.22
+console.log(parseInt(a)) //a=12
+
+a = 12.22
+Math.floor(a)
+
+x=24
+y=String(x)         // returns a string from a number variable x
+y=String(123)       // returns a string from a number literal 123
+y=String(100 + 23)  // returns a string from a number from an expression
+
+
+z=x.toString()
+z=(123).toString()
+z=(100 + 23).toString()
+
