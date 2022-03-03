02cb1b424a7b28c08e1ffb24e6da676fb4535c5c
rahul.gupta <rahul.gupta@rakuten.com>
2022-03-04 01:27:31 +0900

    e11

---
+++ excersice11.js
@@ -0,0 +1,25 @@
+const cars = ["Saab", "Volvo", "BMW"];
+cars[0] = "Opel";
+const cars2 = [];
+cars2[0]= "Saab";
+cars2[1]= "Volvo";
+cars2[2]= "BMW";
+const cars3 = new Array("Saab", "Volvo", "BMW");
+console.log("length of arr is " + car.length)
+console.log(car.indexOf("Volvo"))
+car.push("Maruti")
+car.pop()
+console.log(car1)
+console.log(car2)
+console.log(car3)
+car.unshift("Maruti")
+console.log(arr)
+// shift removes the value at index 0
+car.shift("Volvo")
+console.log(car)
+
+arr = [1, 2, 3, 4, 5, 6]
+console.log(arr)
+console.log(arr.reverse())
+console.log(arr.concat(["1", "2", "3"]))
+console.log(arr.sort())
